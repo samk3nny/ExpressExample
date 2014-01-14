be0ec27a66d2bcd4f8907e8f9d98d31b851ddf06
Sam Kenny <me@samk3nny.com>
2014-01-13 20:57:03 -0500

    Working version

---
+++ app.js
@@ -0,0 +1,35 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+
+app.locals({
+	title: 'Extended Express Example'
+});
+
+app.all('*', function(req, res, next) {
+	fs.readFile('posts.json', function(err, data) {
+		res.locals.posts = JSON.parse(data);
+		next();
+	});
+});
+
+app.get('/', function(req, res) {
+	res.render('arsenal.ejs');
+});
+
+app.get('/post/:slug', function(req, res, next) {
+	res.locals.posts.forEach(function(post) {
+		if (req.params.slug === post.slug) {
+			res.render('post.ejs', { post: post });
+		}
+	})
+});
+
+app.get('api/posts', function(req, res) {
+	res.json(res.locals.posts);
+});
+
+app.listen(3000);
+console.log('app is listening at localhost:3000');
---
+++ readme.md
@@ -0,0 +1,1 @@
+From http://superbigtree.tumblr.com/post/62759231807/a-simple-example-application-using-express-ejs-and
